edb390074bc814903143d2f78d0b96d7837a3fd0
alex <alexguo111@gmail.com>
2023-12-10 22:25:34 +0800

    Initial Commit

---
+++ Udder/UdderApp.swift
@@ -0,0 +1,17 @@
+//
+//  UdderApp.swift
+//  Udder
+//
+//  Created by Alex Guo on 2023/12/10.
+//
+
+import SwiftUI
+
+@main
+struct UdderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
